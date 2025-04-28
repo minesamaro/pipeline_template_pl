1476e303be5e5690202a6dd9c3909f98cc6de031
Maria Amaro <mipaiva@ctm-login-03.inesctec.pt>
2025-04-28 09:46:24 +0100

    Ran experiment

--- slurm_files/shell_script_files/run_experiment_pipeline_job.sh
+++ slurm_files/shell_script_files/run_experiment_pipeline_job.sh
@@ -7,6 +7,12 @@
 #SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
 #SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
 
+init_time=$(date +%Y-%m-%d\ %H:%M:%S)
+echo "Initialization time: $init_time"
+
 # Command-line to run the script for dataset generation.
 srun python3 ../../src/scripts/run_experiment_pipeline.py 
+
+finish_time=$(date +%Y-%m-%d\ %H:%M:%S)
+echo "Finish time: $finish_time"
 
