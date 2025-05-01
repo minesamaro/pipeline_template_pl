a9dd5dc69ddfc431985114cadc284ef8803754ea
minesamaro <22.maria.amaro@gmail.com>
2025-05-01 15:15:41 +0100

    changed 3 to 2

--- src/modules/model/resnet502d/pytorch_lightning_resnet50_2d_model.py
+++ src/modules/model/resnet502d/pytorch_lightning_resnet50_2d_model.py
@@ -2,7 +2,7 @@ from torchmetrics.functional import accuracy, auroc, precision, recall
 import pytorch_lightning
 import torch
 
-from pytorch_lightning_dl_pipeline_template.src.modules.model.resnet502d.resnet503d_model \
+from pytorch_lightning_dl_pipeline_template.src.modules.model.resnet502d.resnet502d_model \
     import ResNet50Model
 from src.modules.loss_functions.resnet50_loss_functions \
     import ResNet50LossFunction
