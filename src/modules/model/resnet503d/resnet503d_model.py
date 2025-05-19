90c93ef691280eaf99e2426fe9d1cff2aa57d59d
minesamaro <22.maria.amaro@gmail.com>
2025-05-19 11:37:57 +0100

    Added gitignore

--- src/modules/model/resnet503d/resnet503d_model.py
+++ src/modules/model/resnet503d/resnet503d_model.py
@@ -8,9 +8,14 @@ class ResNet503DModel(torch.nn.Module):
 
         self.config = config
         self.num_classes = config.number_of_classes
-        self.model = monai_nets.resnet50(pretrained=False)
+        self.model = monai_nets.resnet50(pretrained=False, spatial_dims=3, num_classes=self.num_classes)
         return
 
     def forward(self, model_input):
+        #model_input = model_input.permute(1, 0, 2, 3, 4)
+        model_input = model_input.unsqueeze(1)
+        if model_input.shape[1] != 3:
+            model_input = model_input.repeat(1, 3, 1, 1, 1)
+
         print(f"[DEBUG] Model input shape: {model_input.shape}")
         return self.model(model_input)
