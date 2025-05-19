3fb1b8f71db7801de345de9904f8436c139b9067
Maria Amaro <mipaiva@ctm-login-01.inesctec.pt>
2025-05-19 10:49:16 +0100

    Errors in configs

--- src/modules/data/dataloader/preprocessed_dataloaders/nlst_preprocessed_dataloader.py
+++ src/modules/data/dataloader/preprocessed_dataloaders/nlst_preprocessed_dataloader.py
@@ -435,6 +435,7 @@ class NLSTPreprocessedDataLoader(Dataset):
 
         # Normalize the slice
         dicom_image = self._normalize(dicom_image)
+        print(dicom_image.shape)
 
         return dicom_image
 
