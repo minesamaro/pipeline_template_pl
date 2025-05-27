b2fb996a9ef03ed314323042185e4e885c8188ac
Maria Amaro <mipaiva@ctm-login-01.inesctec.pt>
2025-05-27 14:56:29 +0100

    Fix bottleneck dataloader

---
+++ experiment_results/experiment_6/experiment_info.md
@@ -0,0 +1,4 @@
+Experiment ID: 6
+Who ran it: Maria Amaro
+When was it run: 2025-05-27 13:13
+What was run: ResNEt502d
--- slurm_files/shell_script_files/run_experiment_pipeline_job_test_resnet3d.sh
+++ slurm_files/shell_script_files/run_experiment_pipeline_job_test_resnet3d.sh
@@ -2,8 +2,8 @@
 
 # Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
 #SBATCH --job-name=2d
-#SBATCH --partition=gpu_min32gb
-#SBATCH --qos=gpu_min32gb
+#SBATCH --partition=gpu_min80gb
+#SBATCH --qos=gpu_min80gb
 #SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
 #SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
 
@@ -11,7 +11,7 @@ init_time=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "Initialization time: $init_time"
 
 # Command-line to run the script for dataset generation.
-srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_resnet50_3d
+srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_resnet50_3
 
 finish_time=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "Finish time: $finish_time"
