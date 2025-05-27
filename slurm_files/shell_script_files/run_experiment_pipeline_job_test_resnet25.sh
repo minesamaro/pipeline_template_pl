0953a1437cfb608f8644488e7695955636cf43e6
minesamaro <22.maria.amaro@gmail.com>
2025-05-27 12:17:51 +0100

    Test shell files

---
+++ slurm_files/shell_script_files/run_experiment_pipeline_job_test_resnet25.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
+#SBATCH --job-name=2d
+#SBATCH --partition=gpu_min32gb
+#SBATCH --qos=gpu_min32gb
+#SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
+#SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
+
+init_time=$(date +%Y-%m-%d\ %H:%M:%S)
+echo "Initialization time: $init_time"
+
+# Command-line to run the script for dataset generation.
+srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_resnet50_25
+
+finish_time=$(date +%Y-%m-%d\ %H:%M:%S)
+echo "Finish time: $finish_time"
+
