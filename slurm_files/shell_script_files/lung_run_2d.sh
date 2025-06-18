b8cbfdd4c45d509e46b62f172caa5159573bb441
Maria Amaro <mipaiva@ctm-login-02.inesctec.pt>
2025-06-18 07:25:05 +0100

    Run Lung

---
+++ slurm_files/shell_script_files/lung_run_2d.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
+#SBATCH --job-name=lung2d
+#SBATCH --partition=gpu_min32gb
+#SBATCH --qos=gpu_min32gb
+#SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
+#SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
+
+init_time=$(date +%Y-%m-%d\ %H:%M:%S)
+echo "Initialization time: $init_time"
+
+# Command-line to run the script for dataset generation.
+srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_resnet18_2d_one_class_lung
+srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_resnet50_2d_lung
+srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_vgg16_2d_lung
+
+finish_time=$(date +%Y-%m-%d\ %H:%M:%S)
+echo "Finish time: $finish_time"
+
