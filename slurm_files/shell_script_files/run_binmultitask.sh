297bb7539fbafc75d681d60612e7447a5e12f369
Maria Amaro <mipaiva@ctm-login-02.inesctec.pt>
2025-08-30 00:30:51 +0100

    Running binmultitask

--- slurm_files/shell_script_files/run_binmultitask.sh
+++ slurm_files/shell_script_files/run_binmultitask.sh
@@ -2,8 +2,8 @@
 
 # Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
 #SBATCH --job-name=MT
-#SBATCH --partition=gpu_min80gb
-#SBATCH --qos=gpu_min80gb
+#SBATCH --partition=gpu_min11gb
+#SBATCH --qos=gpu_min11gb
 #SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
 #SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
 
@@ -12,7 +12,7 @@ echo "Initialization time: $init_time"
 
 # Command-line to run the script for dataset generation.
 srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_multitaskbin
-srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_multitask
+#srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_multitask
 
 finish_time=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "Finish time: $finish_time"
