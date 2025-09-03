193bd0a6149ac2d26e62dd4bbeb6d71bc4186164
minesamaro <22.maria.amaro@gmail.com>
2025-09-03 15:43:16 +0100

    Run best vgg16 and save

--- slurm_files/shell_script_files/vgg_best_ws.sh
+++ slurm_files/shell_script_files/vgg_best_ws.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Slurm job execution flags. REQUIRED TO SET: --job-name, --partition and --qos.
-#SBATCH --job-name=bestlungrn18
-#SBATCH --partition=gpu_min12gb
-#SBATCH --qos=gpu_min12gb
+#SBATCH --job-name=bestlungvgg
+#SBATCH --partition=gpu_min11gb
+#SBATCH --qos=gpu_min11gb
 #SBATCH --output=../output_files/JOBNAME=%x_ID=%j.out
 #SBATCH --error=../output_files/JOBNAME=%x_ID=%j.out
 
@@ -11,7 +11,7 @@ init_time=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "Initialization time: $init_time"
 
 # Command-line to run the script for dataset generation.
-srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_best_vgg_ws
+srun python3 ../../src/scripts/run_experiment_pipeline.py --config-name main_vgg16_2d_noda
 
 finish_time=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "Finish time: $finish_time"
